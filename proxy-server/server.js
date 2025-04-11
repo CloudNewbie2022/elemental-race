e2504ac34264976d38bb354e3a1febd83cba1392
CloudNewbie2022 <113952326+CloudNewbie2022@users.noreply.github.com>
2025-04-11 13:56:42 +0000

    change app.use api endpoint

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -59,7 +59,7 @@ app.post('/graphql', async (req, res) => {
       console.log('🔐 Forwarding auth header:', req.headers.authorization);
     }
 
-    const response = await fetch('/https://api-preview.apps.angrydynomiteslab.com/graphql', {
+    const response = await fetch('/graphql', {
       method: 'POST',
       headers: headersToForward,
       body: JSON.stringify(req.body),
